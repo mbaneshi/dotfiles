11945e30ce75148b64fd6936aab9a31503e58c79
Mehdi Baneshi <baneshi@gmail.com>
2022-09-07 21:42:35 +0430

    add color to man page --fish shell

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -82,4 +82,8 @@ starship init fish | source
 
 zoxide init fish | source
 
-
+# Solarized Light & Magenta highlight
+set -g man_blink -o red
+set -g man_bold -o magenta
+set -g man_standout -b white 586e75
+set -g man_underline -u 586e75
